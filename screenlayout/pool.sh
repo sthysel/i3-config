d4171e5def18499d42c58f624c3f02e5c9a4c987
Thys Meintjes <sthysel@gmail.com>
2019-04-30 13:14:09 +0800

    pool and brook

---
+++ screenlayout/pool.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-1.2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output HDMI-0 --off
